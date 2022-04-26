2a792c0d22b4d629246721c907855722f710364e
Kunsty <you@example.com>
2022-04-26 11:36:05 +0700

    Add README.md file

---
+++ DACS3/DACS3/app/src/main/java/com/example/appbanhang/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.appbanhang;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import androidx.appcompat.widget.Toolbar;
+import android.widget.ViewFlipper;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerViewManHinhChinh;
+    NavigationView navigationView;
+    ListView listViewManHinhChinh;
+    DrawerLayout drawerlayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Anhxa();
+        ActionBar();
+        ActionViewFlipper();
+    }
+    private void ActionViewFlipper(){
+
+    }
+    private void ActionBar(){
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerlayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    private void Anhxa() {
+        toolbar = findViewById(R.id.toolbarmanhinhchinh);
+        viewFlipper = findViewById(R.id.viewlipper);
+        recyclerViewManHinhChinh = findViewById(R.id.recyclerview);
+        listViewManHinhChinh = findViewById(R.id.listviewmanhinhchinh);
+        navigationView = findViewById(R.id.navigationview);
+        drawerlayout = findViewById(R.id.drawerlayout);
+
+
+    }
+}

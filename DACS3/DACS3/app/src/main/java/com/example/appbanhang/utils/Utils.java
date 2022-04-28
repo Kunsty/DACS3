57787d045125b82c3afd024a2fc2bb02a9d53805
Kunsty <you@example.com>
2022-04-28 12:55:42 +0700

    new

--- DACS3/DACS3/app/src/main/java/com/example/appbanhang/utils/Utils.java
+++ DACS3/DACS3/app/src/main/java/com/example/appbanhang/utils/Utils.java
@@ -1,5 +1,10 @@
 package com.example.appbanhang.utils;
 
+import com.example.appbanhang.model.GioHang;
+
+import java.util.List;
+
 public class Utils {
-    public static final String BASE_URL="http://192.168.88.143/banhang/";
+    public static final String BASE_URL="http://192.168.55.103/banhang/";
+    public static List<GioHang> manggiohang;
 }
